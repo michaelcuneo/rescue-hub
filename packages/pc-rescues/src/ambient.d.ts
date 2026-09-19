b0e1e314c06dea49fa10e8d29220d85412175c89
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-20 09:52:30 +1000

    fix: correct rescue chat model types

--- packages/pc-rescues/src/ambient.d.ts
+++ packages/pc-rescues/src/ambient.d.ts
@@ -2,68 +2,68 @@
 type Organisations = Organisation[];
 
 type Organisation = {
-  id: string,
-  name: string,
-  financial: boolean,
-  status: string,
-  users: User[],
-  rescues: Rescues[],
-  chats: Chat[],
-}
+	id: string;
+	name: string;
+	financial: boolean;
+	status: string;
+	users: User[];
+	rescues: Rescues[];
+	chats: Chat[];
+};
 
 // Users
 type Users = User[];
 
 type User = {
-  id: string,
-  name: string,
-  email: string,
-  organisations?: Organisation[],
-  avatar: string,
-  chats?: Chat[],
-  rescues?: Rescues[],
-}
+	id: string;
+	name: string;
+	email: string;
+	organisations?: Organisation[];
+	avatar: string;
+	chats?: Chat[];
+	rescues?: Rescues[];
+};
 
 // Chats
 type Chats = Chat[];
 
 type Chat = {
-  id: string,
-  title: string,
-  description: string,
-  status: string,
-  rescueCategory: RescueCategories,
-}
+	id: string;
+	title: string;
+	description: string;
+	status: string;
+	rescueCategory: keyof typeof RescueCategoryEnum;
+};
 
 // Rescues
 type Rescues = Rescue[];
 
 type RescueCategory = {
-  id: string,
-  name: RescueCategoryEnum,
-  description: string,
-  status: string,
-  rescues: Rescues[],
-}
+	id: string;
+	name: RescueCategoryEnum;
+	description: string;
+	status: string;
+	rescues: Rescues[];
+};
 
 type Rescue = {
-  id: string,
-  title: string,
-  description: string,
-  status: string,
-  rescueCategory: RescueCategory,
-  users: User[],
-  organisations: Organisation[],
-}
+	id: string;
+	title: string;
+	description: string;
+	status: string;
+	rescueCategory: RescueCategory;
+	users: User[];
+	organisations: Organisation[];
+};
 
 // Rescue Categories
 enum RescueCategoryEnum {
-  Reptiles,
-  Birds,
-  Mammals,
-  Amphibians,
-  Monotremes,
-  Macropods,
-  Invertebrates,
-  Other,
+	Reptiles,
+	Birds,
+	Mammals,
+	Amphibians,
+	Monotremes,
+	Macropods,
+	Invertebrates,
+	Other
 }
