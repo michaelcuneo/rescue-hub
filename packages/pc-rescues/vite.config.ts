174920592cc7da1d4e0fbd688e5be163e45db37b
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-20 11:41:31 +1000

    fix: restore trusted HTTPS for PC geolocation

--- packages/pc-rescues/vite.config.ts
+++ packages/pc-rescues/vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	server: {
 		port: 3000,
+		strictPort: true,
 		host: '0.0.0.0'
 	},
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), mkcert()]
 });
