4ed53591a1657d8150d1625a155c666e9d5818b5
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-21 13:07:35 +1000

    refactor: remove Cognito infrastructure

--- packages/infra/auth.ts
+++
@@ -1,7 +0,0 @@
-export const users = new sst.aws.CognitoUserPool("RescueHubUsers", {
-  usernames: ["email"],
-});
-
-export const webAuthClient = users.addClient("RescueHubWeb");
-
-export const mobileAuthClient = users.addClient("RescueHubMobile");
