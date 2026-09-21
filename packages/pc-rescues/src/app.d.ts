7a3756a8b11c408fdc7e80706b0815625777d834
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-21 13:08:28 +1000

    feat: type Rescue Hub authenticated session

--- packages/pc-rescues/src/app.d.ts
+++ packages/pc-rescues/src/app.d.ts
@@ -1,16 +1,15 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
+import type { RescueHubUser } from '$lib/server/auth';
+
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			user: RescueHubUser | null;
+		}
 		interface PageState {
 			dialogOpen?: string;
 			accordionSelected?: string;
 		}
-		// interface Platform {}
 	}
 }
 
-export { };
+export {};
